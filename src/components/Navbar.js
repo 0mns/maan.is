2724ae4d889f1a8764a2c181b0a74157646fc60c
Maanis <manis16@outlook.com>
2020-04-27 17:34:13 +0500

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Navbar() {
+  return (
+    <div className="App container">
+      <p>MAANIS</p>
+      <p>CONTACT</p>
+    </div>
+  );
+}
+
+export default Navbar;
